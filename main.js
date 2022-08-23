5577811e21b5d48d5b6e7ba01575efd22d9648a0
황준서 <1201q@naver.com>
2022-08-23 15:53:45 +0900

    mm

--- main.js
+++ main.js
@@ -1,3 +1,17 @@
+let coinsetting = false;
+let observe = false;
+let playing = true;
+const current = {
+  price: [],
+  percent: [],
+};
+
+const coinListArr = {
+  kor: [],
+  eng: [],
+  mar: [],
+};
+
 const TelegramBot = require("node-telegram-bot-api");
 const token = "5374444274:AAGWmVigj2hxeHuLmqdE-8w4lV_Cf8v1O-o";
 const bot = new TelegramBot(token, { polling: true });
@@ -14,8 +28,6 @@ const accoptions = {
   },
 };
 
-let playing = true;
-
 const options = {
   method: "GET",
   url: "https://api.upbit.com/v1/ticker",
@@ -30,13 +42,12 @@ const options2 = {
   headers: { Accept: "application/json" },
 };
 
-// interval
+// interval 기능
 // const interval = setInterval(coinPriceLoad, 1000);
 
 // axios.get 결과값 리턴
 // 인자 p가 무슨 정보 받아올지 결정하는 인자
 // option1이면 시세, 2면 목록
-
 const TestApiCall = async (p) => {
   try {
     const response = await axios.get(p.url, p); // url , request(추가정보)
@@ -45,7 +56,7 @@ const TestApiCall = async (p) => {
     console.log("Error >>", err);
   }
 };
-//
+
 // clg
 // res로 받아옴
 function coinPriceLoad() {
@@ -61,49 +72,71 @@ function coinPriceLoad() {
   });
 }
 
+coinListLoad();
+
 function coinListLoad() {
   TestApiCall(options2).then((res) => {
-    console.log(res.data[0]);
+    for (let i = 0; i < res.data.length; i++) {
+      if (res.data[i].market.includes("KRW")) {
+        coinListArr.kor.push(res.data[i].korean_name);
+        coinListArr.eng.push(res.data[i].english_name);
+        coinListArr.mar.push(res.data[i].market);
+      }
+    }
   });
 }
 
 // 코인 셀렉트
-function coinSelectControl(i) {
-  // options.params["markets"] = coinList[Math.round(Math.random() * 2)];
-  options.params["markets"] = i;
+function coinSelectControl(coinName) {
+  if (coinListArr.mar.includes(coinName)) {
+    coinsetting = true;
+    options.params["markets"] = coinName;
+    console.log("목록에 있음");
+    bot.sendMessage(chatId, `기본코인이 ${coinName}로 설정되었어요.`);
+  } else {
+    console.log("목록에 없음");
+    bot.sendMessage(chatId, `입력하신 코인은 목록에 없어요.`);
+  }
 }
 
-coinPriceLoad();
+bot.onText(/\/btc/, () => {
+  // if (coinsetting === false) {
+  //   bot.sendMessage(chatId, `기본코인을 설정하세요.`);
+  // } else {
+  coinPriceLoad();
+  // }
+});
 
-bot.onText(/\/btc/, coinPriceLoad);
-bot.sendMessage(chatId, `응답할 준비가 되었어요!`);
+//help
+bot.onText(/\/help/, () => {
+  bot.sendMessage(chatId, `1`);
+});
 
-// const button = document.querySelector(".btn");
-// const button2 = document.querySelector(".btn2");
-// const stop = document.querySelector(".stop");
-// const start = document.querySelector(".start");
-// const inputtext = document.querySelector(".inputtext");
-// const inputsubmit = document.querySelector(".inputsubmit");
-// const form = document.querySelector(".submitform");
+//기본코인설정
+bot.onText(/\/코인설정 (.+)/, (msg, match) => {
+  coinSelectControl(match[1].toUpperCase());
+});
 
-// button.addEventListener("click", () => {
-//   coinSelectControl(inputtext.value);
-//   coinListLoad();
-// });
+//코인목록
+bot.onText(/\/코인목록/, () => {
+  if (coinListArr === undefined) {
+    bot.sendMessage(
+      chatId,
+      `아직 코인목록이 로딩되지 않았어요. 조금만 기다려주세요.`
+    );
+  } else {
+    bot.sendMessage(
+      chatId,
+      `현재 원화마켓에서 가능한 코인이에요. \n ${coinListArr.kor}`
+    );
+  }
+});
 
-// start.addEventListener("click", () => {
-//   setInterval(coinPriceLoad, 1000);
-// });
-
-// stop.addEventListener("click", () => {});
-
-// form.addEventListener("submit", (event) => {
-//   event.preventDefault();
-//   coinSelectControl(inputtext.value);
-//   console.log(inputtext.value);
-//   inputtext.value = "";
-// });
-
-// button2.addEventListener("click", () => {
-//   coinPriceLoad();
+// bot.onText(/\/이름 (.+)/, (msg, match) => {
+//   const korIncludes = coinListArr.kor.includes(match[1].toUpperCase());
+//   const engIncludes = coinListArr.eng.includes(match[1].toUpperCase());
+//   const marIncludes = coinListArr.mar.includes(match[1].toUpperCase());
+//   const korIndexof = coinListArr.kor.indexOf(match[1].toUpperCase());
+//   const engIndexof = coinListArr.eng.indexOf(match[1].toUpperCase());
+//   const marIndexof = coinListArr.mar.indexOf(match[1].toUpperCase());
 // });
